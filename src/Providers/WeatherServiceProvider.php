eed39d69b756eacfe446ec04fc0009b3cf220190
Grigory Gerasimov <grigorygerasimov@Grigorys-MacBook-Pro.local>
2023-06-05 17:51:56 +0200

    service-provider init

---
+++ src/Providers/WeatherServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GrigoryGerasimov\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class WeatherServiceProvider extends ServiceProvider
+{
+
+}
