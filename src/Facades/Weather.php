ed4128b3a1b19dc960d1912b57f02fd42ca41b07
Grigory Gerasimov <grigorygerasimov@Grigorys-MacBook-Pro.local>
2023-06-07 16:51:22 +0200

    minor fix in weather facade done

--- src/Facades/Weather.php
+++ src/Facades/Weather.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace GrigoryGerasimov\Weather\Facades;
 
-use GrigoryGerasimov\Weather\Services\WeatherService;
+use GrigoryGerasimov\Weather\Contracts\WeatherServiceInterface;
 use Illuminate\Support\Facades\Facade;
 
 class Weather extends Facade
 {
     protected static function getFacadeAccessor(): string
     {
-        return WeatherService::class;
+        return WeatherServiceInterface::class;
     }
 }
