8bab8e05df0adfdd371fe5f84d215c01ba5d04cb
Grigory Gerasimov <grigorygerasimov@Grigorys-MacBook-Pro.local>
2023-06-16 17:48:02 +0200

    weatherapi reference links added

--- views/weather/layouts/index.blade.php
+++ views/weather/layouts/index.blade.php
@@ -9,5 +9,10 @@
 </head>
 <body>
 @yield('content')
+
+Powered by <a href='https://www.weatherapi.com/' title='Free Weather API'>WeatherAPI.com</a>
+<a href='https://www.weatherapi.com/' title='Free Weather API'>
+    <img src='//cdn.weatherapi.com/v4/images/weatherapi_logo.png' alt='Weather data by WeatherAPI.com' border='0'/>
+</a>
 </body>
 </html>
